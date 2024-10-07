8a9e6e74afda9ba60aa08d01fc9b95950ed8b3de
jakubbonczyk <jakubbonczyk@icloud.com>
2024-10-07 22:59:54 +0200

    Added responsibility and new activity for medicine repository

--- app/src/main/java/com/example/pamietajozdrowiu/YourDrugsActivity.java
+++ app/src/main/java/com/example/pamietajozdrowiu/YourDrugsActivity.java
@@ -1,36 +1,23 @@
 package com.example.pamietajozdrowiu;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class YourDrugsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_your_drugs);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        Button button = findViewById(R.id.button10);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick (View v) {
-                Intent intent = new Intent(MainActivity.this, YourDrugsActivity.class);
-                startActivity(intent);
-            }
         });
     }
 }
