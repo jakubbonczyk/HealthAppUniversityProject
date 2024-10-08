ee8656db3e28b97f1c0f8e0f8cc3737eb08cf975
jakubbonczyk <jakubbonczyk@icloud.com>
2024-10-09 01:08:30 +0200

    Added back button

--- app/src/main/java/com/example/pamietajozdrowiu/YourDrugsActivity.java
+++ app/src/main/java/com/example/pamietajozdrowiu/YourDrugsActivity.java
@@ -1,6 +1,10 @@
 package com.example.pamietajozdrowiu;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -18,6 +22,15 @@ public class YourDrugsActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button button = findViewById(R.id.button4);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(YourDrugsActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
