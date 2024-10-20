bf9833bac1b1f7c1000b10f6b4c4e9059afb528c
jakubbonczyk <jakubbonczyk@icloud.com>
2024-10-20 15:23:37 +0200

    added buttons to users feelings

--- app/src/main/java/com/example/pamietajozdrowiu/YourFeelingsActivity.java
+++ app/src/main/java/com/example/pamietajozdrowiu/YourFeelingsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,12 +26,25 @@ public class YourFeelingsActivity extends AppCompatActivity {
         });
 
         Button button = findViewById(R.id.button4);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(YourFeelingsActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
+        Button sadButton = findViewById(R.id.button5);
+        Button neutralButton = findViewById(R.id.button6);
+        Button happyButton = findViewById(R.id.button7);
+
+        button.setOnClickListener(v -> {
+            Intent intent = new Intent(YourFeelingsActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        sadButton.setOnClickListener(v -> {
+            Toast.makeText(this, "Czujesz się źle!", Toast.LENGTH_SHORT).show();
+        });
+
+        neutralButton.setOnClickListener(v -> {
+            Toast.makeText(this, "Czujesz się neutralnie!", Toast.LENGTH_SHORT).show();
+        });
+
+        happyButton.setOnClickListener(v -> {
+            Toast.makeText(this, "Czujesz się świetnie!", Toast.LENGTH_SHORT).show();
         });
     }
 }
